751323fbf1743a78c5a15cfd7ff7bc697b111ae7
Douglas Martins <32877533+dougls@users.noreply.github.com>
2025-08-05 20:36:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Olá! Esta aplicação foi implantada com um pipeline de CI/CD com IA da FIAP!</h1>"
+    return "<h1>Olá! Esta aplicação foi implantada com um pipeline de CI/CD com IA da FIAP, em Agosto de 2025!</h1>"
 
 @app.route('/health')
 def health_check():
-    return jsonify(status="ok", message="Aplicação está saudável."), 200
+    return jsonify(status="ok", message="Aplicação está saudável para cluster EKS."), 200
 
 @app.route('/version')
 def app_version():
-    image_version = os.environ.get("IMAGE_VERSION", "não definida")
+    image_version = os.environ.get("IMAGE_VERSION", "N/A")
     return jsonify(version=image_version)
 
 if __name__ == '__main__':
